0bf197466497b97efb3a9bd62086b96463d047ac
Ian Linsmeier <19559173+ilinsmeier@users.noreply.github.com>
2022-12-28 15:10:32 -0500

    finish saveNote method

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -8,7 +8,21 @@ export default class NotesAPI {
     }
 
     static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
+        const existing = notes.find(note => note.id == noteToSave.id)
 
+        // Edit/Update
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
 
     static deleteNote(id) {
