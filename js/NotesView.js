bc8252d06dd9967af2853e210af2a0800a79fe1e
Ian Linsmeier <19559173+ilinsmeier@users.noreply.github.com>
2022-12-28 18:04:47 -0500

    add preliminary notes view

---
+++ js/NotesView.js
@@ -0,0 +1,19 @@
+export default class NotesView {
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+            <div class="notes__sidebar">
+                <button class="notes__add" type="button">Add Note</button>
+                <div class="notes__list"></div>
+            </div>
+            <div class="notes__preview">
+                <input class="notes__title" type="text" placeholder="New Note...">
+                <textarea class="notes__body"Take Note...</textarea>
+            </div>
+        `;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-import NotesAPI from "./NotesAPI.js"
+// import NotesAPI from "./NotesAPI.js"
 
-NotesAPI.saveNote({
-    title: "New Note!",
-    body: "I am a new note."
+// NotesAPI.saveNote({
+//     title: "New Note!",
+//     body: "I am a new note."
+// });
+
+// console.log(NotesAPI.getAllNotes());
+
+import NotesView from "./NotesView.js"
+
+const app = document.getElementById("app");
+const view = new NotesView(app, {
+    onNoteSelect() {
+        console.log("Note has been selected!");
+    }
 });
-
-console.log(NotesAPI.getAllNotes());
