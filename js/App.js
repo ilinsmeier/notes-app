4766b28ce51b6d1e338fe42a2fc30d3b4ace11be
Ian Linsmeier <19559173+ilinsmeier@users.noreply.github.com>
2022-12-29 14:38:31 -0500

    add handlers for note select, add, edit, & delete

--- js/App.js
+++ js/App.js
@@ -4,6 +4,23 @@ export default class App {
     constructor(root) {
         this.notes = [];
         this.activeNote = null;
-        this.view = new NotesView(root);
+        this.view = new NotesView(root, this._handlers());
+    }
+
+    _handlers() {
+        return {
+            onNoteSelect: noteId => {
+                console.log("Note selected: " + noteId);
+            },
+            onNoteAdd: () => {
+                console.log("Note Add");
+            },
+            onNoteEdit: (title, body) => {
+                console.log(title, body);
+            },
+            onNoteDelete: noteId => {
+                console.log("Note DELETED: " + noteId);
+            },
+        };
     }
 }
